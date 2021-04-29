4ec724afc2775ed14e4c3b81523c923a69d3a8d2
Michael <kodes.michal@seznam.cz>
2021-04-29 23:36:21 +0200

    fixed gramma mistakes

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const names = ['Paulie', 'Sergio','Mikael','Pedro','Toby','Josh','George','John'];
 const activities = ['played','ate','kissed','broke','crushed','pet','made','cleaned','threw','cut','slapped'];
 const objects = ['a chair','an apple','a dog','a knife','a cat','a car','a window','a desk','a laptot','a sink','a toilet','a trashcan','a beer','an ice-cream'];
-const places = ['in the kitchen','in the park','in the living room','on the garden','in the bathroom','in the garage','on the playground', 'at the office','at the square'];
+const places = ['in the kitchen','in the park','in the living room','on the garden','in the bathroom','in the garage','on the playground', 'in the office','on the square'];
 
 //function to help with getting random words
 function randomWord (arr) {
@@ -18,7 +18,3 @@ let place = randomWord(places);
 
 //printing the random message to terminal
 console.log(person + ' '+activity+' '+ object +' '+ place+'.');
-
-
-
-
