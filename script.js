21d0c5912f5b8f988bfe636135069b94bdbeca9d
Michael <kodes.michal@seznam.cz>
2021-04-29 23:13:38 +0200

    created arrays with words

---
+++ script.js
@@ -0,0 +1,4 @@
+const names = ['Paulie', 'Sergio','Mikael','Pedro','Toby','Josh','George','John'];
+const activities = ['played','ate','kissed','broke','crushed','pet','made','cleaned','threw','cut','slapped'];
+const objects = ['a chair','an apple','a dog','a knife','a cat','a car','a window','a desk','a laptot','a sink','a toilet','a trashcan','a beer','an ice-cream'];
+const places = ['in the kitchen','in the park','in the living room','on the garden','in the bathroom','in the garage','on the playground', 'at the office','at the square'];
